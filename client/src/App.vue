968261f4c74f9c6c712ab3b06563a3627e483be4
Bruno De Angelis <41843151+brunodeangelis@users.noreply.github.com>
2019-08-01 23:45:00 -0300

    scaffold

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="font-sans">
+    <div class="mt-32 flex flex-col items-center">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+        alt="logo instagram"
+        class="w-48"
+      />
+      <h1 class="text-xl text-gray-500 tracking-wide mt-8">PUPPETEER INSTAGRAM PROFILE FETCHER</h1>
+      <input type="text" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style>
+</style>
